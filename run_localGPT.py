a1e388361c784943be9d6578188c5ed8ba86f84d
Kumar PSR <kumarpsr9@gmail.com>
2023-07-31 11:20:19 +0000

    Llma2 Model Added

--- run_localGPT.py
+++ run_localGPT.py
@@ -220,11 +220,14 @@ def main(device_type, show_sources):
     # model_basename = "wizard-vicuna-13B.ggmlv3.q2_K.bin"
     # model_id = "TheBloke/orca_mini_3B-GGML"
     # model_basename = "orca-mini-3b.ggmlv3.q4_0.bin"
+    
+    model_id = "meta-llama/Llama-2-7b-chat-hf"
+    #model_basename = "llama-2-7b-chat-hf"
+     
+    #model_id = "TheBloke/Llama-2-7B-Chat-GGML"
+    #model_basename = "llama-2-7b-chat.ggmlv3.q4_0.bin"
 
-    model_id = "TheBloke/Llama-2-7B-Chat-GGML"
-    model_basename = "llama-2-7b-chat.ggmlv3.q4_0.bin"
-
-    llm = load_model(device_type, model_id=model_id, model_basename=model_basename)
+    llm = load_model(device_type, model_id=model_id) #, model_basename=model_basename)
 
     qa = RetrievalQA.from_chain_type(llm=llm, chain_type="stuff", retriever=retriever, return_source_documents=True)
     # Interactive questions and answers
